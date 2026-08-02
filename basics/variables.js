d1347002e03924c55e515a4830031d9d49632746
Neha <mehtaneha822@gmail.com>
2026-08-02 11:40:17 +0530

    changes

--- basics/variables.js
+++ basics/variables.js
@@ -2,3 +2,12 @@ const accId=144
 let accemail="mehta@123.com"
 var accPss="12345"
 accountcity="Cmpt"
+
+
+// accId=2
+console.log(accId)
+
+accemail="nehaneha"
+accPss="2222"
+console.log(accemail)
+console.log(accPss)
