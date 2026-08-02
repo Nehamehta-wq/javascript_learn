a80be3f0b7858b03d710c397f0941477f0e2aa1c
Neha <mehtaneha822@gmail.com>
2026-08-02 17:17:11 +0530

    new

--- basics/04_conversion.js
+++ basics/04_conversion.js
@@ -11,12 +11,98 @@ let isloggedIn="Neha"
 // let loggedIn=Number(isloggedIn)
 let loggedIn=Boolean(isloggedIn)
 // let loggedIn=String(isloggedIn)
-console.log(loggedIn)
+// console.log(loggedIn)
 
 
 let loggedin=""
 let logIn=Number(loggedin)
 // console.log(logIn)
+
+
+// String Operations
+let str1="Hello"
+let str2="World"
+let str3=str1+str2
+// console.log(str3)
+
+
+//length
+// let str = "JavaScript";
+// console.log(str.length);
+
+
+//Convert to Uppercase
+// let str = "helloneha";
+// console.log(str.toUpperCase());
+
+// //character to idx
+// console.log(str.charAt(2));
+
+// // idx of char
+// console.log(str.indexOf("S"));
+
+
+// //slice
+// console.log(str.slice(0,4));
+
+
+// //substring
+// console.log(str.substring(4,10));
+
+
+// // replace
+// // let str = "I like Java";
+// // console.log(str.replace("Java","Python"));
+
+// //replace all
+// console.log(str.replaceAll("cat","dog"));
+
+
+// //includes
+// console.log(str.includes("Script"));
+
+// //ends with
+// console.log(str.endsWith("Script"));
+
+
+
+// //split
+// let str = "Apple,Banana,Mango";
+// console.log(str.split(","));
+
+
+// //Trim
+// let str = "   Hello   ";
+// console.log(str.trim());
+
+
+// //Repeat
+// let str = "Hi ";
+// console.log(str.repeat(3));
+
+
+// //Compare Strings
+// let a = "Apple";
+// let b = "Apple";
+// console.log(a === b);
+
+
+
+
+// console.log(1 + 2 + "3") // 33
+// console.log("3" + 1 + 2) // 312
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
