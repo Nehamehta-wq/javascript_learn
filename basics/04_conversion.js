cc38bba479f159e508922d2e23c221ca96a5c1de
Neha <mehtaneha822@gmail.com>
2026-08-02 15:02:24 +0530

    conversion

---
+++ basics/04_conversion.js
@@ -0,0 +1,31 @@
+let score="Neha"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let value=Number(score)
+// console.log(typeof value)
+// console.log(value)
+
+
+let isloggedIn="Neha"
+// let loggedIn=Number(isloggedIn)
+let loggedIn=Boolean(isloggedIn)
+// let loggedIn=String(isloggedIn)
+console.log(loggedIn)
+
+
+let loggedin=""
+let logIn=Number(loggedin)
+// console.log(logIn)
+
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// 1 => true 
+// 0 => false
+// "" => false
+// "hye" => true
