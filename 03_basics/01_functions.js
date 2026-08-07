b2999d2633a1d66c795341139dc2ce95bafba2e6
Neha <mehtaneha822@gmail.com>
2026-08-07 19:44:11 +0530

    new

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+// A function is a reusable block of code that performs a specific
+//  task. Instead of writing the same code multiple times, you write it once in a 
+//  function and call it whenever needed.
+
+
+function saymyname(){
+    console.log("N");
+    console.log("E");
+    console.log("H");
+    console.log("A");
+}
+
+//saymyname(); // Calling the function to execute its code
+
+function addtwonumbers(num1, num2){
+    console.log(num1 + num2);
+}   
+addtwonumbers(5, 10); // Calling the function with arguments 5 and 10
